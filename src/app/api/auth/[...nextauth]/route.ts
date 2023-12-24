6442c297cc91319a4d0aac79dce02940506d21f5
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-12-24 10:47:55 +0530

    hello-d

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ import { authOptions } from "@/utils/auth";
 
 import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
